e856b346d3f37ca254984bad33b5dabf5711cae2
Afanasjew Vladislav <afanasjew.vladick@yandex.ru>
2021-01-07 17:04:25 +0300

    #refactor

--- src/App.js
+++ src/App.js
@@ -1,36 +1,11 @@
-import React, { useContext } from 'react';
-import { AppBar, AppBarTitle, AppBarAction, MoreVertSVGIcon } from 'react-md';
+import React from 'react';
 import SnakeGameWrapper from './pages/SnakeGameWrapper';
-import { SnakeGameContext } from './context/SnakeGameContext';
+import Header from './components/header';
 
 const App = () => {
-  const { count, maxCount } = useContext(SnakeGameContext);
-
   return (
     <section>
-      <AppBar id="simple-usage-app-bar" theme="primary">
-        <AppBarTitle
-          id="simple-usage-title"
-          className="rmd-typography--capitalize"
-        >
-          Змейка
-        </AppBarTitle>
-        <AppBarTitle
-          id="simple-usage-title"
-          className="rmd-typography--capitalize"
-        >
-          Текущий счет: {count}
-        </AppBarTitle>
-        <AppBarTitle
-          id="simple-usage-title"
-          className="rmd-typography--capitalize"
-        >
-          Рекорд: {maxCount}
-        </AppBarTitle>
-        <AppBarAction id="simple-usage-search" first aria-label="Search">
-          <MoreVertSVGIcon />
-        </AppBarAction>
-      </AppBar>
+      <Header />
       <SnakeGameWrapper />
     </section>
   );
---
+++ src/components/header.js
@@ -0,0 +1,29 @@
+import React, { useContext } from 'react';
+import { AppBar, AppBarTitle, AppBarAction, MoreVertSVGIcon } from 'react-md';
+import { SnakeGameContext } from '../context/SnakeGameContext';
+
+const Header = () => {
+  const { count, maxCount } = useContext(SnakeGameContext);
+
+  return (
+    <AppBar id="simple-usage-app-bar" theme="primary">
+      <AppBarTitle
+        id="simple-usage-title"
+        className="rmd-typography--capitalize"
+      >
+        Текущий счет: {count}
+      </AppBarTitle>
+      <AppBarTitle
+        id="simple-usage-title"
+        className="rmd-typography--capitalize"
+      >
+        Рекорд: {maxCount}
+      </AppBarTitle>
+      <AppBarAction id="simple-usage-search" first aria-label="Search">
+        <MoreVertSVGIcon />
+      </AppBarAction>
+    </AppBar>
+  );
+};
+
+export default Header;
